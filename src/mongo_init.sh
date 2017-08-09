b8290e732a8ac077a961fb55db1fe9335ba0f077
angela-baby <angela_baby@aliyun.com>
2017-08-10 00:01:36 +0800

    vim

--- src/mongo_init.sh
+++ src/mongo_init.sh
@@ -1,15 +1,15 @@
 mongo 
 use admin;
 db.createUser(
-{
-	user: "ichunt",
-	pwd: "huntmon66499",
-	roles:
-	[
-	  {
-		role: "readWrite",
-		db: "ichunt"
-	  }
-	]
-	}
+    {
+        user: "ichunt",
+        pwd: "huntmon66499",
+        roles:
+        [
+            {
+                role: "readWrite",
+                db: "ichunt"
+            }
+        ]
+    }
 );
